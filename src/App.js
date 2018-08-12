5a931422ce9a2a0fc9a2a16e228598e582b64755
Akim Desire <adesi006.ng@gmail.com>
2018-08-11 20:55:13 -0400

    functional

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import MainMenu from "./components/main-menu";
 import CharacterCreate from "./components/character-create";
 import BattlePage from "./components/battle/battle-page";
+import VictoryPage from "./components/battle/victory-page";
+import DeathPage from "./components/battle/death-page";
 import "./App.css";
 import { connect } from "react-redux";
 
@@ -13,6 +15,12 @@ class App extends Component {
     }
     if (this.props.page === "battle") {
       return <BattlePage />;
+    }
+    if (this.props.page === "victory") {
+      return <VictoryPage />;
+    }
+    if (this.props.page === "death") {
+      return <DeathPage />;
     }
     return <MainMenu />;
   }
